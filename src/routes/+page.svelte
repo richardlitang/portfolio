bc665a7503ad4ee8375db26a6aa23eea849f36ae
Richard Litang <richardjasonlitang@gmail.com>
2023-02-26 23:31:17 +0800

    install svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,65 @@
+<script>
+</script>
+
+<div class="center">
+	<div class="hero">
+		<h1>Hi, I'm Richard</h1>
+		<h2>I'm a developer</h2>
+	</div>
+</div>
+
+<div>
+	<p>I want to build things that move us forward</p>
+	<p>Or not, it's fines</p>
+</div>
+
+<!-- Transition to expand on experience on scroll -->
+<div>
+	<div>
+		<p>I'm currently working as a full stack dev at a startup that aims to simplify healthcare. </p>
+	</div>
+	<div>
+		<p>Before becoming a developer, I actually worked in a power plant</p>
+	</div>
+</div>
+
+<!-- Transition to become compact again -->
+<div>
+	<p>I'm an engineer at heart, I enjoy solving real world problems with </p>
+	<p>I'm open to exploring and learning about new industries, but these currently excite me:</p>
+	<div>
+		<p>Sustainability</p>
+		<p>Healthcare</p>
+		<p>Education</p>
+	</div>
+</div>
+
+<div style="display: grid; grid-template-columns: 1fr 1fr; grid-gap: 1em">
+	<div>
+		<h2>Keyed</h2>
+	</div>
+</div>
+
+<div>
+	<p>Show technologies here</p>
+	<div>
+		<p>Professional</p>
+	</div>
+	<div>
+		<p>Amateur</p>
+	</div>
+</div>
+		
+<style>
+	.center {
+		margin: auto;
+		width: 50%;
+	}
+
+	.hero {
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+	}
+
+</style>
